934d38ac7bfd08c018c77db01fc59b9c5df8a492
samlarson94 <samlarson94@gmail.com>
2022-06-22 10:04:03 -0500

    Adding header section and social icons

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,17 @@
 import Link from 'next/link';
 import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin, AiFillVideoCamera } from 'react-icons/ai';
 import { DiCssdeck } from 'react-icons/di';
 
 //Import styled components from HeaderStyles.js
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+import { Container, Div1, Div2, Div3, NavLink, SocialIcons, Span } from './HeaderStyles';
 
 const Header = () =>  (
   <Container>
     <Div1>
       <Link href="/">
-        <a style={{ display: 'flex', alignItems: "center", color: 'white' }}>
-          <DiCssdeck size="3rem" /> <span> Portfolio </span>
+        <a style={{ display: 'flex', alignItems: "center", color: 'white', marginBottom: '20px'}}>
+          <DiCssdeck size="3rem" /> <Span> Sam Larson </Span>
         </a>
       </Link>
     </Div1>
@@ -23,9 +23,31 @@ const Header = () =>  (
         </Link>
       </li>
       <li>
-        
+      <Link href="#tech">
+          <NavLink>Technologies</NavLink>
+        </Link>
+      </li>
+      <li>
+      <Link href="#about">
+          <NavLink>About</NavLink>
+        </Link>
       </li>
       </Div2>
+
+      <Div3>
+        <SocialIcons href="https://github.com/samlarson94">
+          <AiFillGithub size="3rem" />
+        </SocialIcons>
+        <SocialIcons href="https://www.linkedin.com/in/sam-larson-minneapolis/">
+          <AiFillLinkedin size="3rem" />
+        </SocialIcons>
+        <SocialIcons href="https://www.instagram.com/samlarsonfilms.fg/">
+          <AiFillInstagram size="3rem" />
+        </SocialIcons>
+        <SocialIcons href="https://www.studio31g.com">
+          <AiFillVideoCamera size="3rem" />
+        </SocialIcons>
+      </Div3>
   
   </Container>
 );
