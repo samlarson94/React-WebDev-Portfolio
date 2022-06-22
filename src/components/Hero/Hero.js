24289258aa3582f6d36e78d04bb42ea98474cd66
samlarson94 <samlarson94@gmail.com>
2022-06-22 10:15:01 -0500

    Adding hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,24 @@
 import React from 'react';
 
+//Import general styled components from Global Components
 import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
+//Import Hero-specific components from HeroStyles.js
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+ <Section row nopadding>
+   <LeftSection>
+     <SectionTitle main center>
+      Sam Larson <br />
+      Full-Stack Web Developer
+     </SectionTitle>
+     <SectionText>
+       Former Construction Manager turned Web Developer.  Well versed in front-end JavaScript development and various frameworks.  Building on the back-end with Node, Express, and MongoDB.
+     </SectionText>
+     <Button onClick={() => window.location = "mailto:samlarson94@gmail.com"}>Learn More</Button>
+   </LeftSection>
+ </Section>
 );
 
 export default Hero;
