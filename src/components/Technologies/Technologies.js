49823ea31c7b8929a8ab7ca7d3df565dd8f7b1fb
samlarson94 <samlarson94@gmail.com>
2022-06-22 12:55:31 -0500

    Adding Technologies section

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,58 @@
 import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import { DiCompass, DiFirebase, DiReact, DiZend } from 'react-icons/di';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+  <Section id="tech">
+    <SectionDivider />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      I've worked with a range of technologies in the web development world, from front-end to back-end.
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiReact size="3rem" />
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            React.js, JavaScript, HTML/CSS
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size="3rem" />
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Node.js, Express.js, MongoDB, MySQL, GraphQL, Sequelize, Insomnia/Postman
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiZend size="3rem" />
+        <ListContainer>
+          <ListTitle>UI/UX</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            tools like Figma, Adobe Creative Suite, Canva
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiCompass size="3rem" />
+        <ListContainer>
+          <ListTitle>Project Management</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            tools like Microsoft Project, Microsoft Office Suite
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+    </Section>
 );
 
 export default Technologies;
