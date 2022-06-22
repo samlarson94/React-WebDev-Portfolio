13abe46ca8d8ebc053e8368a34975610b9195120
samlarson94 <samlarson94@gmail.com>
2022-06-22 10:26:28 -0500

    Adding projects section.  Mapping over projects as separate objects.

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,9 +5,30 @@ import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalCompon
 import { projects } from '../../constants/constants';
 
 const Projects = () => (
-  <div>
-    Projects
-  </div>
+  <Section nopadding id="projects">
+    <SectionDivider />
+    <SectionTitle main>Projects</SectionTitle>
+    <GridContainer>
+      {[{
+        title: 'Project 1',
+        description: 'Description Here',
+      },{
+        title: 'Project 2',
+        description: 'Description Here',
+      },{
+        title: 'Project 3',
+        description: 'Description Here',
+      },{
+        title: 'Project 4',
+        description: 'Description Here',
+      }].map((project) => (
+        <div>
+          {project.title} <br />
+          {project.description}
+        </div>
+      ))}
+    </GridContainer>
+    </Section>
 );
 
 export default Projects;
