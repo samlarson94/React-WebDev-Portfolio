1b0ba6aab9d07287f067d16b183494feea4eea6e
samlarson94 <samlarson94@gmail.com>
2022-06-22 11:17:34 -0500

    Building out project information in constants folder. Recent projects have been added.

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,28 +4,56 @@ import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag,
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
 import { projects } from '../../constants/constants';
 
+const projectInformation = [{
+  title: 'Project 1',
+  description: 'Description Here',
+  image: "", 
+  tags: [],
+  source: "",
+  visit: "",
+  id: 0,
+},{
+  title: 'Project 2',
+  description: 'Description Here',
+  image: "", 
+  tags: [],
+  source: "",
+  visit: "",
+  id: 1,
+},{
+  title: 'Project 3',
+  description: 'Description Here',
+  image: "", 
+  tags: [],
+  source: "",
+  visit: "",
+  id: 2,
+},{
+  title: 'Project 4',
+  description: 'Description Here',
+  image: "", 
+  tags: [],
+  source: "",
+  visit: "",
+  id: 3,
+}];
+
+
 const Projects = () => (
   <Section nopadding id="projects">
     <SectionDivider />
     <SectionTitle main>Projects</SectionTitle>
     <GridContainer>
-      {[{
-        title: 'Project 1',
-        description: 'Description Here',
-      },{
-        title: 'Project 2',
-        description: 'Description Here',
-      },{
-        title: 'Project 3',
-        description: 'Description Here',
-      },{
-        title: 'Project 4',
-        description: 'Description Here',
-      }].map((project) => (
-        <div>
-          {project.title} <br />
-          {project.description}
-        </div>
+      {projects.map(({ id, image, title, description, tags, source, visit }) => (
+        <BlogCard key = {id}>
+          <Img src={image} />
+          <TitleContent>
+            <HeaderThree title>{title}</HeaderThree>
+          </TitleContent>
+          <CardInfo>
+            {description}
+          </CardInfo>
+        </BlogCard>
       ))}
     </GridContainer>
     </Section>
